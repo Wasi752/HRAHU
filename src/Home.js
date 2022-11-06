c387769b38ef47ab29669ebe360dc0c059258a18
https://github.com/Wasi752 <osiur2@gmail.com>
2022-11-06 22:36:25 +0600

    HR Modiul

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+    const cName = "border border-blue-300 w-[50%] p-2 ml-40 mb-2 text-xl hover:text-blue-600";
+    return (
+        <div className="w-full h-screen flex">
+            <div className="w-[25%] h-full"></div>
+            <div className="flex flex-col justify-center m-5 w-[50%] h-[70%] mt-20 bg-gray-100">
+            <p className="text-5xl text-justify-center m-10 text-blue-500">Official Work Flow of Al-Haiatul Ulya</p>
+                <Link to="/form" className={cName}>Staff List</Link>
+                <Link to="/counter" className={cName}>Event</Link>
+                <Link to="/fetch" className={cName}>Work Flow</Link>
+                <Link to="/time" className={cName}>Pay Scal</Link>
+                <Link to="/color" className={cName}>Rulls & Regulation</Link>
+                <Link to="/bulb" className={cName}>Office Time & Present Count</Link>
+                <Link to="/effect" className={cName}>Recreation</Link>
+            </div>
+            <div className="w-[25%] h-full"></div>
+        </div>
+    )
+};
+export default Home;
---
+++ src/StaffList.js
@@ -0,0 +1,44 @@
+const staff_data = [
+    {
+    name: "Mawlana Muhammad Ismayeel", 
+    designation: "Najim_e_Emtihan", 
+    contact_no: "01913500453", 
+    e_mail: "haiatululya@gmail.com", 
+    room_no: 301
+}, 
+{
+    name: "Mawlana Md Osiur Rahman", 
+    designation: "Office Secretary", 
+    contact_no: "01305656703", 
+    e_mail: "osiur2@gmail.com", 
+    room_no: 201
+},
+{
+    name: "Mawlana Md Abdul Malik", 
+    designation: "Head of Accounts", 
+    contact_no: "01935650801", 
+    e_mail: "haiatululya@gmail.com", 
+    room_no: 203
+},
+{
+    name: "Mawlana Md Abdul Malik", 
+    designation: "Head of Accounts", 
+    contact_no: "01935650801", 
+    e_mail: "haiatululya@gmail.com", 
+    room_no: 203
+},
+{
+    name: "Md Sharif Hossain", 
+    designation: "Senior Assistant", 
+    contact_no: "01935650801", 
+    e_mail: "haiatululya@gmail.com", 
+    room_no: 203
+},
+{
+    head_of_accounts: "Mawlana Md Abdul Malik", 
+    name_of_designation: "Head of Accounts", 
+    contact_no: "01935650801", 
+    e_mail: "haiatululya@gmail.com", 
+    room_no: 203
+},
+]
