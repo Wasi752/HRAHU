f90bd2ca055fcdfdfabbf3b44689ff036c1626a6
https://github.com/Wasi752 <osiur2@gmail.com>
2022-11-12 02:07:51 +0600

    Input New Data in Staff Details

--- src/Form.js
+++ src/Form.js
@@ -2,58 +2,55 @@
 import { useState } from "react";
 import data from "./data";
 
-function Form(){
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setMobileNumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [users, setUsers] = useState(data);
+function Form({setX}){
+    const [nationality, setNationality] = useState("");
+    const [religion, setRrligion] = useState("");
+    const [brith, setBrith] = useState("");
+    const [nid, setNid] = useState("");
 
     const submit = ()=>{
         const user = {
-            name: name, 
-            email: email, 
-            phone: phone, 
-            password: password
+            nationality: nationality, 
+            religion: religion, 
+            brith: brith, 
+            nid: nid
         };
-        setUsers([...users, user]);
+        setX(user);
     };
     return(
         <div className="w-full h-screen ">
             
-            
-                    
                 <div className="w-full h-4/6 bg-gray-100 p-5 ">        
                     <p className="mt-1 ml-5 text-left underline mr-10">Input Information</p>
-                    <div className="mt-1 flex"><label className="ml-1 pr-1 mt-7 ">User Name :</label>    
+                    <div className="mt-1 flex"><label className="ml-4 pr-2 mt-7 ">Nationality :</label>    
                         <input 
                             type="text"
-                            value= {name} 
-                            onChange={e => setName(e.target.value)}  
+                            value= {nationality} 
+                            onChange={e => setNationality(e.target.value)}  
                             className="h-[10%] w-[65%] p-4 border border-blue-300 rounded-md m-3"
                         />
                     </div>
-                    <div className="mt-1 flex"><label className="ml-8 pr-3 pl-1 mt-7">Email :</label>           
+                    <div className="mt-1 flex"><label className="ml-8 pr-3 pl-1 mt-7">Religion :</label>           
                         <input 
                             type="text" 
-                            value= {email} 
-                            onChange={e => setEmail(e.target.value)} 
+                            value= {religion} 
+                            onChange={e => setRrligion(e.target.value)} 
                             className="h-[10%] w-[65%] p-4 border border-blue-300 rounded-md m-3"
                         />
                     </div>
-                    <div className="mt-1 flex"><label className="ml-4 pr-4 pl-3 mt-7">Phone :</label>   
+                    <div className="mt-1 flex"><label className="ml-4 pr-4 pl-3 mt-7">Birth Day :</label>   
                         <input 
                             type="text" 
-                            value= {phone} 
-                            onChange={e => setMobileNumber(e.target.value)} 
+                            value= {brith} 
+                            onChange={e => setBrith(e.target.value)} 
                             className="h-[10%] w-[65%] ml-2 p-4 border border-blue-300 rounded-md m-3"
                         />
                     </div>
-                    <div className="mt-1 flex"><label className=" ml-2 pr-2 mt-7">Password :</label>        
+                    <div className="mt-1 flex"><label className=" ml-9 pr-4 mt-7">NID No :</label>        
                         <input 
                             type="password" 
-                            value= {password} 
-                            onChange={e => setPassword(e.target.value)} 
+                            value= {nid} 
+                            onChange={e => setNid(e.target.value)} 
                             className="h-[10%] w-[65%] p-4 border border-blue-300 rounded-md m-3"
                         />
                     </div>
@@ -61,13 +58,12 @@ function Form(){
                         <button 
                             type="button"
                             onClick={submit} 
-                            className="text-center text-1xl text-white font-bold h-[80%] w-[90%] ml-6 bg-blue-600 hover:text-blue-300 p-3 border rounded-md ">Submit</button>
+                            className="text-center text-1xl text-white font-bold h-[80%] w-[85%] ml-10 bg-blue-600 hover:text-blue-300 p-3 border rounded-md ">Submit</button>
                     </div>
                 </div>
                 
            
             
-                <p>{JSON.stringify(users)}</p>
         </div>
     );
 }
--- src/StaffDetail.js
+++ src/StaffDetail.js
@@ -5,7 +5,7 @@ import Form from "./Form";
 import data from "./data";
 
 
-function StaffDetail({ i, id, name, father, mother, present_address, permanent_address, academic_achievement, languages_skills, designation, contact_no, e_mail, room_no, image }) {
+function StaffDetail({ i, id, name, father, mother, present_address, permanent_address, academic_achievement, languages_skills, designation, contact_no, e_mail, room_no, user, image }) {
 
     return (
         <div className="flex flex-row">
@@ -22,7 +22,10 @@ function StaffDetail({ i, id, name, father, mother, present_address, permanent_a
                 <p className="font-serif">{contact_no}</p>
                 <p className="font-serif">{e_mail}</p>
                 <p className="font-serif">{room_no}</p>
-
+                <p className="font-serif">{user?.nationality}</p>
+                <p className="font-serif">{user?.religion}</p>
+                <p className="font-serif">{user?.brith}</p>
+                <p className="font-serif">{user?.nid}</p>
             </div>
             <div className="w-[30%] h-[35%] ml-40 mt-10 justify-right ">
                 <img src={"/" + image} className="w-[90%] h-[80%] ml-1" />
@@ -37,46 +40,46 @@ function StaffDetails() {
     const idNo = parseInt(menu);
     const [pageNo, setPageNo] = useState(1);
     const [staff, setStaff] = useState();
-    const [users, setUsers] = useState(data);
+    const [user, setUser] = useState();
 
     useEffect(() => {
         fetch('https://mocki.io/v1/10e2cef1-2965-41d8-b94e-984e14deb6de')
             .then((response) => response.json())
             .then((data) => {
-                const y = data
-                    .filter((x) => x.id === idNo)
-                    .map((b, i) =>
-                        <StaffDetail
-                            i={i + 1 + (1 * (pageNo - 1))}
-                            id={b.id}
-                            name={b.name}
-                            father={b.father}
-                            mother={b.mother}
-                            present_address={b.present_address}
-                            permanent_address={b.permanent_address}
-                            academic_achievement={b.academic_achievement}
-                            languages_skills={b.languages_skills}
-                            designation={b.designation}
-                            contact_no={b.contact_no}
-                            e_mail={b.e_mail}
-                            room_no={b.room_no}
-                            image={b.image} />);
-
-                setStaff(y);
+                const x = data[0];
+                const y = data.filter((x) => x.id === idNo)[0];
+                setStaff(
+                    <StaffDetail
+                        i={y.id}
+                        id={y.id}
+                        name={y.name}
+                        father={y.father}
+                        mother={y.mother}
+                        present_address={y.present_address}
+                        permanent_address={y.permanent_address}
+                        academic_achievement={y.academic_achievement}
+                        languages_skills={y.languages_skills}
+                        designation={y.designation}
+                        contact_no={y.contact_no}
+                        e_mail={y.e_mail}
+                        room_no={y.room_no}
+                        image={y.image}
+                        user={user} />
+                );
             });
-    }, [pageNo]);
+    }, [pageNo, user]);
 
     return (
         <div className="w-full h-full flex">
             <div className="w-[30%] h-full flex flex-row mt-20">
-                <Form/>
+                <Form setX={setUser} />
             </div>
             <div className="flex flex-col ml-5 w-[60%] h-[80%] mt-10 bg-gray-100">
                 <p className="text-2xl font-serif font-extrabold ml-10 mb-5 mt-5 text-blue-500">Staff Details</p>
 
                 <div>
                     <p className="justify-left">{staff}</p>
-                    
+
                 </div>
                 <div>
                     <Link to={"/sdetails/" + (idNo - 1)} >
